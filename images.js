68700ce8cbdde7c1d15bb4d3c43098de8176a0e2
trust-kimberly <nguyen.kimmay@gmail.com>
2024-11-12 20:48:32 -0800

    abstract images list

---
+++ images.js
@@ -0,0 +1,24 @@
+const carouselImages = {
+  images: [
+    { src: "/media/baby_window.jpg" },
+    { src: "/media/Project_Las Vegas/161008_VegasProject_LLV_01.jpg", active: true },
+    { src: "/media/220412_UrbanFarmsLA_0245.jpg" },
+    { src: "/media/240528_TwoBunch_1350.jpg" },
+    { src: "/media/220718_Sequoia_204-1.jpeg" },
+    { src: "/media/Lizzy2.jpeg" },
+    { src: "/media/parsley.jpg" },
+    { src: "/media/240528_TwoBunch_089.jpg" },
+    { src: "/media/220616_UFLA_6059Springvale_162.jpeg" },
+    { src: "/media/221015_KaitCulmo_033.jpg" },
+    { src: "/media/240528_TwoBunch_827.jpg" },
+    { src: "/media/221219_UFLA_1501HillDr_191.jpeg" },
+    { src: "/media/hawaii_limo.jpg" },
+    { src: "/media/220830_GiaWolff_MagnoliaStudio_448.jpeg" },
+    { src: "/media/220412_UrbanFarmsLA_0049.jpg" },
+    { src: "/media/220507_Hawaii_148.jpeg" },
+    { src: "/media/221105_Zora_535.jpeg" },
+    { src: "/media/Trousdale.jpg" },
+    { src: "/media/240528_TwoBunch_1013.jpg" },
+    { src: "/media/20230723_GW_Effie_0270.jpg" }
+  ]
+}; 
