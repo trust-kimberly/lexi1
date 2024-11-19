4167b5842a99bb8a27768bdc7a7c866f271e0870
Alexandra <alexandra@Alexandras-MBP.attlocal.net>
2024-11-18 20:45:51 -0800

    cleaned up code  and added left/right scroll to image pop ups

--- images.js
+++ images.js
@@ -1,6 +1,6 @@
 const images = [
-    { src: "media/baby_window.jpg", tags: ["landing"], caption: "Baby Window" },
-    { src: "media/Project_Las Vegas/161008_VegasProject_LLV_01.jpg", tags: ["landing"], active: true, caption: "Las Vegas Project" },
+    { src: "media/baby_window.jpg", tags: ["landing"] },
+    { src: "media/Project_Las Vegas/161008_VegasProject_LLV_01.jpg", tags: ["landing"], active: true },
     { src: "media/220412_UrbanFarmsLA_0245.jpg", tags: ["landing"] },
     { src: "media/240528_TwoBunch_1350.jpg", tags: ["landing"] },
     { src: "media/220718_Sequoia_204-1.jpeg", tags: ["landing", "landscapes"] },
